b445c40c06f25c3565172f1e4d6756411b50ff1a
maciektbt4 <mbkrzak@gmail.com>
2022-08-15 12:13:41 +0200

    Factorial function update - check how built in function is efficient

--- beginer/calculate_factorial.py
+++ beginer/calculate_factorial.py
@@ -1,16 +1,32 @@
 from math import factorial
-from time import time, time_ns
+# from time import time, time_ns
 
-sizeList = (10, 100, 1000, 10000, 10000, 100000, 1000000)
-timeExecutionList = []
+# def calcFactorialAndTimeExec(number, startTime):
+#     sum_factorial = 1
 
-sum_factorial = 1
+#     for i in range(number):
+#        sum_factorial *= i + 1
+#     return sum_factorial, time() - startTime
 
-for n in sizeList:
-    startTime = time()
-    for i in range(n):
-        sum_factorial *= (i + 1)
-    timeExecutionList.append(time() - startTime)
 
-print(f"Execution time list is: {timeExecutionList} in [s]")
-#print(f"End time: {time()}")
+# sizeList = (10, 100, 1000, 10000, 10000, 100000, 1000000)
+# timeExecutionList = []
+# results = []
+# timeExecutionListFactorialFunction = []
+
+# for n in sizeList:
+#     result, execTime = calcFactorialAndTimeExec(n, time())
+#     timeExecutionList.append(execTime)
+#     results.append(result)
+
+# for n in sizeList:
+#     startTime = time()
+#     factorial(n)
+#     timeExecutionListFactorialFunction.append(time() - startTime)
+
+# print("Factorial to calculate from following numbers: ", sizeList)   
+# print("Times execution for user defined function:\n", timeExecutionList)
+# print("Times execution for build in function:\n", timeExecutionListFactorialFunction)
+
+with open("factorial_from_milion.txt","w") as fp:
+    fp.writelines(str(factorial(1000000)))
