f2a0c70383f8fc985dea9c9cf52795c9a2cfdb26
maciektbt4 <mbkrzak@gmail.com>
2022-08-24 17:10:21 +0200

    Correct naming convention for miscellaneous_project to mit PEP8

--- miscellaneous_projects/correct_names/correct_names.py
+++ miscellaneous_projects/correct_names/correct_names.py
@@ -1,47 +1,71 @@
+"""
+This is class which contain all necessary methods to change names.
+
+It check corectness of files in source path rename it to proper
+names and save them in destination path.
+"""
+
 import os
 import re
 
 class ManageFiles:
+    """Class representing a file managment."""
 
     def __init__(self, source_path, destination_path) -> None:
+        """
+        Set up source and destination path.
+
+        Args:
+            source_path: Path to files to change.
+            destination_path: Path where files with changed name should be saved
+        """
         self.source_path = source_path
         self.destination_path = destination_path
-    
-    def changeCurrentDir(self):
+
+    def change_current_dir(self):
+        """Change current location to defined in source path."""
         os.chdir(self.source_path)
-    
-    def printCurrentDir(self):
+
+    def print_current_dir(self):
+        """Print current location path."""
         print(os.getcwd())
 
-    def listFolderElements(self):
+    def list_folder_elements(self):
+        """List files in current location and return them in list."""
         print(os.listdir())
         return os.listdir()
 
-    def createTestFiles(self, num = 10):
+    def create_test_files(self, num = 10):
+        """Create testing files. By default 10 files are created."""
         for i in range(1, num):
             filename = str(i) + ".py"
-            with open(filename, "w") as file:
+            with open(filename, "w", encoding="utf-8") as file:
                 file.write(f"That's file name named {filename}")
-    
-    def findFilesWithWrongNames(self, files_list):
+
+    def find_files_with_wrong_names(self, files_list):
+        """Check which files have incorrect names."""
         # Define regex pattern to find only wrong names - names with 1 decimal and .py end
         regex_pattern = r"\d.py"
         matches = []
-        for f in files_list:
+        for file in files_list:
             # Check if regex pattern is matched and return value is not none
-            if re.match(regex_pattern, f) is not None:
-                matched_object = re.match(regex_pattern, f)
+            if re.match(regex_pattern, file) is not None:
+                matched_object = re.match(regex_pattern, file)
                 # Append only values from matched object returned from re.match
                 matches.append(matched_object.group(0))
         return matches
 
-    def renameFilesWithCorrectNames(self, files):
+    def rename_files_with_correct_names(self, files):
+        """Correct names.
+
+        Add '0' on the begining of file - eg. '1.py' is changing to '01.py'
+        """
         for file in files:
             corrected_name = "0" + file
             os.replace(file, corrected_name)
 
-
-    def removeFiles(self, *file):
-        for f in file:
-            file_path = self.source_path + str(r"/" + f)
+    def remove_files(self, *folder_path):
+        """Remove testing files (all from current location)."""
+        for file in folder_path:
+            file_path = self.source_path + str(r"/" + file)
             os.remove(file_path)
--- miscellaneous_projects/correct_names/main.py
+++ miscellaneous_projects/correct_names/main.py
@@ -1,21 +1,44 @@
+"""
+This is an script for automatic renaming incorrect file names.
+
+It check corectness of files in source path rename it to proper
+names and save them in destination path.
+"""
+
 from correct_names import ManageFiles
 
+# Ask user to define source and destination paths
 # source_path = input("Pass source path: ")
 # destination_path = input("Pass destination path: ")
-source_path = r"/home/maciek/Documents/Python/test"
-destination_path = r"/home/maciek/Documents/Python/test"
 
-file_manager = ManageFiles(source_path, destination_path)
+# For test reason we are using fixed paths
+SOURCE_PATH = r"/home/maciek/Documents/Python/test"
+DESTINATION_PATH = r"/home/maciek/Documents/Python/test"
 
-file_manager.changeCurrentDir()
-file_manager.printCurrentDir()
-file_manager.createTestFiles(num=30)
+file_manager = ManageFiles(SOURCE_PATH, DESTINATION_PATH)
 
-folder_elements = file_manager.listFolderElements()
-matches  = file_manager.findFilesWithWrongNames(folder_elements)
+# Change current location to source path location
+file_manager.change_current_dir()
+
+# Print location path
+file_manager.print_current_dir()
+
+# Create 30 test files in current location
+file_manager.create_test_files(num=30)
+
+# Save in list files from current location
+folder_elements = file_manager.list_folder_elements()
+
+# Check and save in list, files with incorrect names
+matches  = file_manager.find_files_with_wrong_names(folder_elements)
 print(matches)
 
-file_manager.renameFilesWithCorrectNames(matches)
-folder_elements = file_manager.listFolderElements()
+# Correct file names 
+file_manager.rename_files_with_correct_names(matches)
 
-file_manager.removeFiles(*folder_elements)
+# Save in list folder elements
+folder_elements = file_manager.list_folder_elements()
+print(folder_elements)
+
+# Add the end remove all test files
+file_manager.remove_files(*folder_elements)
